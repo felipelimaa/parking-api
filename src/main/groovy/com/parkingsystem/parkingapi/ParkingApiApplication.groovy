c21617d2690814d8616e0a5b77ae4ca4b5d1b95b
felipe <fearlimasi@gmail.com>
2022-07-10 12:29:47 -0300

    Initial commit

---
+++ src/main/groovy/com/parkingsystem/parkingapi/ParkingApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.parkingsystem.parkingapi
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ParkingApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ParkingApiApplication, args)
+	}
+
+}
