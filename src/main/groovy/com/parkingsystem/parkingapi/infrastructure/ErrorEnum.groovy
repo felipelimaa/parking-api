a5f16991783fe7df316460bb1c31f0592845ad3d
felipe <fearlimasi@gmail.com>
2022-07-15 21:36:13 -0300

    feat: create update method from organization
    Refs: PARKING-9

--- src/main/groovy/com/parkingsystem/parkingapi/infrastructure/ErrorEnum.groovy
+++ src/main/groovy/com/parkingsystem/parkingapi/infrastructure/ErrorEnum.groovy
@@ -6,6 +6,7 @@ enum ErrorEnum {
     ERROR_WHILE_REGISTER_ORGANIZATION('Something went wrong while register Organization.'),
     ERROR_WHILE_FINDING_ORGANIZATION('Something went wrong while finding Organization.'),
     ERROR_WHILE_GENERATE_ORGANIZATION_ID('Something went wrong while generate Organization_ID.'),
+    ERROR_WHILE_UPDATE_ORGANIZATION('Something went wrong while update Organization.'),
     FIELD_ORGANIZATION_NAME_NOT_DECLARED("Please inform 'name'."),
     FIELD_ORGANIZATION_COST_NOT_DECLARED("Please inform 'cost'."),
     FIELD_ORGANIZATION_MAXIMUM_CAPACITY_NOT_DECLARED("Please inform 'maximum_capacity'."),
